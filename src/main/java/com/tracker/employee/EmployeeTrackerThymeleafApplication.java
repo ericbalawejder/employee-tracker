2ed8175eb9abc8fc0f4dfb439b308339e6f1fffb
Eric Balawejder <eric.balawejder@gmail.com>
2021-01-05 15:32:23 -0500

    Initial content from spring initialzr

---
+++ src/main/java/com/tracker/employee/EmployeeTrackerThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.tracker.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeTrackerThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeTrackerThymeleafApplication.class, args);
+    }
+
+}
