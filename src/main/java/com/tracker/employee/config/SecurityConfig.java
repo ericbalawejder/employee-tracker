24343209cce683593b88da62b8772780dccc4162
Eric Balawejder <eric.balawejder@gmail.com>
2021-01-05 23:52:47 -0500

    Added Spring security login

---
+++ src/main/java/com/tracker/employee/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.tracker.employee.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("securityDataSource")
+    private DataSource securityDataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // use jdbc authentication
+        auth.jdbcAuthentication().dataSource(securityDataSource);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/employees/showForm*").hasAnyRole("MANAGER", "ADMIN")
+                .antMatchers("/employees/save*").hasAnyRole("MANAGER", "ADMIN")
+                .antMatchers("/employees/delete").hasRole("ADMIN")
+                .antMatchers("/employees/**").hasRole("EMPLOYEE")
+                .antMatchers("/resources/**").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .exceptionHandling()
+                .accessDeniedPage("/access-denied");
+    }
+
+}
